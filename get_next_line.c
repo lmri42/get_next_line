19256596544e238dd8f88ab8c05a582f0ed45ccf
lmri42 <157985088+lmri42@users.noreply.github.com>
2024-03-30 03:15:48 +0100

    im done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: luribero <luribero@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 23:50:43 by luribero          #+#    #+#             */
-/*   Updated: 2024/03/30 02:40:58 by luribero         ###   ########.fr       */
+/*   Updated: 2024/03/30 03:12:33 by luribero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,20 +44,20 @@ char *get_next_line(int fd)
 	{
 	    if((eloc(remain) + 1) < ft_strlen(remain))
 	    {
-		line = ft_substr(remain, 0, eloc(remain) + 1);
+		line = ft_substr(remain, 0, eloc(remain) + 1); //line=malloc
 		temp = remain;
-		remain = ft_substr(remain, eloc(remain) + 1, ft_strlen(remain) - eloc(remain));
-		free(temp);
+		remain = ft_substr(remain, eloc(remain) + 1, ft_strlen(remain) - eloc(remain)); //new_remain=malloc
+		free(temp); //old_remain=free
 	    }
 	    else
 	    {
-	    	line = ft_substr(remain, 0, eloc(remain) + 1);
-		free(remain);
+	    	line = ft_substr(remain, 0, eloc(remain) + 1); //line=malloc
+		free(remain); //remain=free
 		remain = 0;
 	    }
 	    return (line);
 	}
-	bag = ft_strdup(remain);
+	bag = ft_strdup(remain); //bag=malloc
     }
     cread = read(fd, buffer, BUFFER_SIZE);
     if(cread == -1)
@@ -73,12 +73,12 @@ char *get_next_line(int fd)
 		if(ft_strlen(bag) != 0)
 		{
 	    		temp = bag;
-			bag = ft_strjoin(bag,buffer);
-	    		free(temp);
+			bag = ft_strjoin(bag,buffer); //new_bag=malloc
+	    		free(temp); //old_bag=free
 		}
 		else
 		{
-		    bag = ft_strdup(buffer);
+		    bag = ft_strdup(buffer); //bag=malloc
 		}
 		cread = read(fd, buffer, BUFFER_SIZE);
 		if(cread == -1)
@@ -89,49 +89,49 @@ char *get_next_line(int fd)
 	}
 	if (eloc(bag) > -1)
 	{
-	    line = ft_substr(buffer, 0, eloc(buffer) + 1);
+	    line = ft_substr(buffer, 0, eloc(buffer) + 1); //line=malloc
 	    if((eloc(bag) + 1) < ft_strlen(bag))
 	    {
-		remain = ft_substr(bag, eloc(bag) + 1, ft_strlen(bag) - eloc(bag));
+		remain = ft_substr(bag, eloc(bag) + 1, ft_strlen(bag) - eloc(bag)); //remain=malloc
 	    }
 	    else
 	    {
-		free(remain);
+		free(remain); //remain=free
 		remain = 0;
 	    }
 	}
 	else if ((eloc(buffer) > -1) && (ft_strlen(bag) > 0))
 	{
 	    temp = bag;
-	    bag = ft_strjoin(bag,buffer);
-	    free(temp);
-	    line = ft_substr(bag, 0, eloc(bag) + 1);
+	    bag = ft_strjoin(bag,buffer); //new_bag=malloc
+	    free(temp); //old_bag=free
+	    line = ft_substr(bag, 0, eloc(bag) + 1); //line=malloc
 	    if((eloc(buffer) + 1) < ft_strlen(buffer))
 	    {
-		remain = ft_substr(buffer, eloc(buffer) + 1, ft_strlen(buffer) - eloc(buffer));
+		remain = ft_substr(buffer, eloc(buffer) + 1, ft_strlen(buffer) - eloc(buffer)); //remain=malloc
 	    }
 	    else
 	    {
-		free(remain);
+		free(remain); //remain=free
 		remain = 0;
 	    }
 	}
 	else
 	{
-	    line = ft_substr(buffer, 0, eloc(buffer) + 1);
+	    line = ft_substr(buffer, 0, eloc(buffer) + 1); //line=malloc
 	    if((eloc(buffer) + 1) < ft_strlen(buffer))
 	    {
-		remain = ft_substr(buffer, eloc(buffer) + 1, ft_strlen(buffer) - eloc(buffer));
+		remain = ft_substr(buffer, eloc(buffer) + 1, ft_strlen(buffer) - eloc(buffer)); //remain=malloc
 	    }
 	    else
 	    {
-		free(remain);
+		free(remain); //remain=free
 		remain = 0;
 	    }
 	}
 	return (line);
     }
-    free(remain);
+    free(remain); //remain=free
     remain = 0;
     return(0);
 } 
