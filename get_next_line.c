fe8918b552115f1e94ee5cd8c899a5fb695bc46c
lmri42 <157985088+lmri42@users.noreply.github.com>
2024-04-17 13:44:04 +0200

    i can taste it

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: luribero <luribero@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 23:50:43 by luribero          #+#    #+#             */
-/*   Updated: 2024/04/17 12:02:19 by luribero         ###   ########.fr       */
+/*   Updated: 2024/04/17 13:39:53 by luribero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ char	*get_next_line(int fd)
 		{
 //			printf("END OF LINE FOUND IN REMAIN!\n"); //testing
 //			printf("Eloc (remain) in \"%d\"\n", eloc(remain)); //testing
-			line = ft_substr(remain, 0, eloc(remain)); //CHECK
+			line = ft_substr(remain, 0, eloc(remain));
 //			printf("1.Line says \"%s\"\n", line); //testing
 
 			if ((eloc(remain) + 1) < ft_strlen(remain))
@@ -66,7 +66,7 @@ char	*get_next_line(int fd)
 		else
 		{
 //			printf ("CREATING BAG FROM REMAIN"); //testing
-			bag = ft_substr(remain, 0, ft_strlen(remain)); //CHECK
+			bag = ft_substr(remain, 0, ft_strlen(remain));
 //			printf("Strlen(remain) is \"%d\"\n", ft_strlen(remain)); //testing
 //			printf("Bag says \"%s\"\n", bag); //testing
 			free(remain);
@@ -87,7 +87,7 @@ char	*get_next_line(int fd)
 		}
 		else
 		{
-			bag = ft_substr(buffer, 0, ft_strlen(buffer)); //CHECK
+			bag = ft_substr(buffer, 0, ft_strlen(buffer));
 //			printf("Bag says \"%s\"\n", bag); //testing
 //			printf("Strlen(buffer) is \"%d\"\n", ft_strlen(buffer)); //testing
 		}
@@ -111,35 +111,30 @@ char	*get_next_line(int fd)
 		else
 			return (0);
 	}
-	else if ((cread > 0) && (cread < BUFFER_SIZE))
+	else if ((cread > 0) && (cread < BUFFER_SIZE)) // CHECK
 	{
 		if ((bag != 0) && (*bag != '\0'))
 		{
-			line = ft_strjoin(bag, buffer);
-			free (bag);
+			line = ft_strjoin (bag, buffer);
+			free(bag);
 			bag = 0;
 		}
 		else
+			line = ft_substr (buffer, 0, ft_strlen(buffer));
+		if (eloc(line) > -1)
 		{
-			if(eloc(buffer) > -1)
-			{
-				line = ft_substr(buffer, 0, eloc(buffer)); //CHECK
-				if ((eloc(buffer) + 1) < (ft_strlen(buffer)))
-				{
-					remain = ft_substr(buffer, eloc(buffer), ft_strlen(buffer) - eloc(buffer));
-//					printf ("Remains for next time:\n\"%s\"\n", remain);//testing
-				}
-			}
-			else
-				line = ft_substr(buffer, 0, ft_strlen(buffer)); //CHECK
-//			printf("2.Line says \"%s\"\n", line); //testing
-//			printf("Strlen(buffer) is \"%d\"\n", ft_strlen(buffer)); //testing
+			temp = line;
+			line = ft_substr (line, 0, eloc(line));
+			if((eloc(buffer) + 1) < ft_strlen(buffer))
+				remain = ft_substr(temp, eloc(temp), ft_strlen(temp) - eloc(temp));
+			free(temp);
+			temp = 0;
 		}
 		return (line);
 	}
 	else
 	{
-		line = ft_substr(buffer, 0, eloc(buffer)); //CHECK
+		line = ft_substr(buffer, 0, eloc(buffer));
 //		printf("3.Line says \"%s\"\n", line); //testing
 //		printf("Eloc(buffer) is \"%d\"\n", eloc(buffer)); //testing
 		if ((eloc(buffer) + 1) < (ft_strlen(buffer)))
